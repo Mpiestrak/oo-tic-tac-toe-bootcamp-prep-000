fecad4f18cb6c98989934bf31e9935b60cd09cc7
Matthew Piestrak <mpiestrak@gmail.com>
2018-06-10 02:19:00 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,9 +1,10 @@
-class TicTacToe
-  def initialize
-   @board = Array.new(9, " ")
+class TicTacToe 
+  
+  def initialize 
+    @board = Array.new(9, " ")
   end
-
-WIN_COMBINATIONS = [
+  
+  WIN_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
   [6, 7, 8],
@@ -12,8 +13,9 @@ WIN_COMBINATIONS = [
   [2, 5, 8],
   [0, 4, 8],
   [6, 4, 2]
-  ]
-def display_board
+    ]
+  
+  def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
   puts "-----------"
   puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
@@ -21,23 +23,24 @@ def display_board
   puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
 end
 
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
 
-def move(index, current_player = "X")
+  def input_to_index(user_input)
+    user_input.to_i - 1
+  end
+  
+  def move(index, current_player = "X")
   @board[index] = current_player
-end
-
-def position_taken?(index)
-  !@board[index].nil? || @board[index] == " ")
-end
-
-def valid_move?(index)
+  end
+  
+  def position_taken?(index)
+  !(@board[index].nil? || @board[index] == " ")
+  end
+  
+  def valid_move?(index)
   index.between?(0,8) && !position_taken?(index)
-end
-
-def turn
+  end
+  
+  def turn
   puts "Please choose a number 1-9:"
   user_input = gets.chomp
   index = input_to_index(user_input)
@@ -48,9 +51,9 @@ def turn
   else
     turn
   end
-end
-
-def turn_count
+  end
+  
+  def turn_count
   turn = 0
   @board.each do |index|
     if index == "X" || index == "O"
@@ -58,9 +61,9 @@ def turn_count
     end
   end
   return turn
-end
-
-def current_player
+  end
+  
+  def current_player
   num_turns = turn_count
   if num_turns % 2 == 0
     player = "X"
@@ -68,25 +71,25 @@ def current_player
     player = "O"
   end
   return player
-end
+  end
 
 def won?
   WIN_COMBINATIONS.each {|win_combo|
-  index_0 = win_combo[0]
-  index_1 = win_combo[1]
-  index_2 = win_combo[2]
+    index_0 = win_combo[0]
+    index_1 = win_combo[1]
+    index_2 = win_combo[2]
 
-position_1 = @board[index_0]
-position_2 = @board[index_1]
-position_3 = @board[index_2]
+    position_1 = @board[index_0]
+    position_2 = @board[index_1]
+    position_3 = @board[index_2]
 
-if position_1 == "X" && position_2 == "X" && position_3 == "X"
-  return win_combo
-elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-  return win_combo
-end
-}
-return false
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combo
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combo
+    end
+  }
+  return false
 end
 
 def full?
@@ -119,20 +122,21 @@ def winner
       return "X"
     else
       return "O"
+    end
   end
-end
 end
 
 def play
   until over? == true
-  turn
-end
+    turn
+  end
 
-if won?
-  puts "Congratulations #{winner}!"
+  if won?
+    puts "Congratulations #{winner}!"
   elsif draw?
-  puts "Cat's Game!"
+    puts "Cat's Game!"
+  end
 end
-end
-
+  
+  
 end
